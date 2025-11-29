37eaee335a14a52ca209600867d41040462d7049
Karsten Horsmann <khorsmann@gmail.com>
2025-11-29 22:26:27 +0100

    fix: update mqtt stub for wattwächter and tasmota

--- internal/mqtt/mqtt.go
+++ internal/mqtt/mqtt.go
@@ -2,6 +2,7 @@ package mqtt
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"strconv"
 	"strings"
@@ -45,13 +46,90 @@ func StartClient(cfg config.Config, db *sql.DB) mqtt.Client {
 // ---------------- Handler Stubs ----------------
 
 func handleWattwaechter(topic, payload string, db *sql.DB, cfg config.Config) {
-	// TODO: Wattwächter-Logik implementieren
 	log.Printf("[Wattwaechter] %s = %s", topic, payload)
-}
 
+	var msg struct {
+		Time string `json:"Time"`
+		E320 struct {
+			EIn   float64 `json:"E_in"`
+			EOut  float64 `json:"E_out"`
+			Power int     `json:"Power"`
+		} `json:"E320"`
+	}
+
+	if err := json.Unmarshal([]byte(payload), &msg); err != nil {
+		log.Printf("[Wattwaechter] JSON Fehler: %v", err)
+		return
+	}
+
+	// Zeit konvertieren
+	t, err := time.Parse(time.RFC3339, msg.Time)
+	if err != nil {
+		log.Printf("[Wattwaechter] Zeitformatfehler: %v", err)
+		t = time.Now()
+	}
+
+	stmt, err := db.Prepare(`
+		INSERT INTO energy_data (timestamp_unix, timestamp_rfc3339, e_in, e_out, power)
+		VALUES (?, ?, ?, ?, ?)`)
+	if err != nil {
+		log.Printf("[Wattwaechter] DB Prepare Fehler: %v", err)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
+		t.Unix(),
+		msg.Time,
+		msg.E320.EIn,
+		msg.E320.EOut,
+		msg.E320.Power,
+	)
+
+	if err != nil {
+		log.Printf("[Wattwaechter] DB Insert Fehler: %v", err)
+	} else if cfg.Broker.SetDebug {
+		log.Printf("[Wattwaechter] gespeichert E_in=%f E_out=%f Power=%d",
+			msg.E320.EIn, msg.E320.EOut, msg.E320.Power)
+	}
+}
 func handleTasmota(topic, payload string, db *sql.DB, cfg config.Config) {
-	// TODO: Tasmota-Logik implementieren
 	log.Printf("[Tasmota] %s = %s", topic, payload)
+
+	var msg struct {
+		Time   string `json:"Time"`
+		Energy struct {
+			Power float64 `json:"Power"`
+		} `json:"ENERGY"`
+	}
+
+	if err := json.Unmarshal([]byte(payload), &msg); err != nil {
+		log.Printf("[Tasmota] JSON Fehler: %v", err)
+		return
+	}
+
+	t, err := time.Parse(time.RFC3339, msg.Time)
+	if err != nil {
+		log.Printf("[Tasmota] Zeitformatfehler: %v", err)
+		t = time.Now()
+	}
+
+	stmt, err := db.Prepare(`
+		INSERT INTO tasmota_data (device_id, timestamp_unix, timestamp_rfc3339, power)
+		VALUES (?, ?, ?, ?)
+	`)
+	if err != nil {
+		log.Printf("[Tasmota] DB Prepare Fehler: %v", err)
+		return
+	}
+	defer stmt.Close()
+
+	deviceID := strings.Split(topic, "/")[1]
+
+	_, err = stmt.Exec(deviceID, t.Unix(), msg.Time, msg.Energy.Power)
+	if err != nil {
+		log.Printf("[Tasmota] DB Insert Fehler: %v", err)
+	}
 }
 
 func handleSolar(topic, payload string, db *sql.DB, cfg config.Config) {
